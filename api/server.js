e7e728a6dd6a566f2ee90ac5cda8869ca4332a3d
Daniel <33533130+danieltsam@users.noreply.github.com>
2025-03-05 00:17:10 +1000

    lets go v2

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 require('dotenv').config();
 
